1761046a9c7762ec53224bc3727784c85048f237
phx-hp1000 <306761352@qq.com>
2013-06-04 13:45:44 +0800

    pageneedvirified page 文字修改

--- application/views/site/pageneedverified.blade.php
+++ application/views/site/pageneedverified.blade.php
@@ -7,7 +7,9 @@
 <div class="container">
     <div class="row">
         <div class="span12">
-        	<h4>对不起，您要查看的页面需要您的帐号通过审核后才能查看。</h4>
+        	<h4>您好！请到“控制面板”里完成
+        		个人资料的填写，等待帐号通过审核后才能搜索其他用户。</h4>
+        	<a class="btn btn-primary" href="{{ URL::to_route('dashboard') }}">现在就去</a>
         </div>
     </div>
 </div>
