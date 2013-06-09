bf12cdcdfac5a10bd9223f1137d796058de90b2f
Huang Feng <306761352@qq.com>
2013-06-09 11:20:07 +0800

    add color change for principles

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
-	// 隐藏footer
-	$('.footer').show();
+    // 隐藏footer
+    $('.footer').show();
 
-	// 对IE版本进行检测
-	if($.browser.msie != undefined){
-		$('#browser-promote').show();
-	};
+    // 对IE版本进行检测
+    if($.browser.msie != undefined){
+        $('#browser-promote').show();
+    };
 
-	// 原则自动变色
-	var colorScheme = [['#ff4040', '#ff7373', '#ff0000', '#bf3030', '#a60000'],
-					   ['#ffb240', '#ffc773', '#ff9900', '#bf8630', '#a66300'],
-					   ['#37da7e', '#62da97', '#00b64f', '#22884f', '#007633'],
-					   ['#3476cd', '#5dc8cd', '#01939a', '#1d7074', '#006064'],
-					   ['#4671d5', '#6c8cd5', '#1240ab', '#2a4480', '#06266f'],
-					   ['#9f3ed5', '#ad66d5', '#7109aa', '#5f2580', '#48036f'],
-					   ['#d60062', '#4312ae', '#154f55', '#d42b28', '#6c43ce']];
-	console.log(colorScheme);
-	var changeColor = window.setInterval(function (){
-	    $('#difference > .span2:nth-child(1)').hide();
+    // 几大原则自动变色
+    // 暂时不用
+    /*
+    var colorScheme = [['#ff4040', '#ff7373', '#ff0000', '#bf3030', '#a60000'],
+                       ['#ffb240', '#ffc773', '#ff9900', '#bf8630', '#a66300'],
+                       ['#37da7e', '#62da97', '#00b64f', '#22884f', '#007633'],
+                       ['#3476cd', '#5dc8cd', '#01939a', '#1d7074', '#006064'],
+                       ['#4671d5', '#6c8cd5', '#1240ab', '#2a4480', '#06266f']];
 
+    var changeColor = window.setInterval(function (){
+        // 生成随机数(0-4), 即上面的一维数组数
+        var randomIndex = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
 
-	}, 1000);
+        for (var i = 0; i < 5; i++) {
+            $('#difference > .span2:nth-child('+(i+1)+')')
+                .css('background-color', colorScheme[randomIndex][i]);
+        };
 
+    }, 10000);
+    */
 
 });
