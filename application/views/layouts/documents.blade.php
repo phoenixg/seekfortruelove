91dc3f815b84a6ba2d397a8810d49dbd52da3ed6
phoenix@hp321 <306761352@qq.com>
2013-06-11 09:37:06 +0800

    add cnzz stats

--- application/views/layouts/documents.blade.php
+++ application/views/layouts/documents.blade.php
@@ -80,5 +80,9 @@
 
 
     @yield('page_scripts_footer')
+    <!-- CNZZ STATS -->
+    <div style="display:none;visibility:hidden;">
+        <script src="http://s13.cnzz.com/stat.php?id=4858222&web_id=4858222" language="JavaScript"></script>
+    </div>
 </body>
 </html>
