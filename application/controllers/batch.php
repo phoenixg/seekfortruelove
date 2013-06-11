eec48951d835a5ce2283f15bc9ff6a97745622bb
phoenix@hp321 <306761352@qq.com>
2013-06-11 16:22:43 +0800

    邮件

--- application/controllers/batch.php
+++ application/controllers/batch.php
@@ -7,8 +7,12 @@ class Batch_Controller extends Base_Controller
     public function get_sendmail()
     {
         set_time_limit(0);
+
         $mailer = Laravel\IoC::resolve('mailer');
         $messageBody = file_get_contents('/srv/www/seekfortruelove.org/public_html/material/email-template/1.html');
+
+        $GLOBALS['mailer']      = & $mailer;
+        $GLOBALS['messageBody'] = & $messageBody;
 
         $emails = explode(PHP_EOL, file_get_contents('/srv/www/seekfortruelove.org/public_html/material/emails/20130611_1.txt'));
        
@@ -17,8 +21,8 @@ class Batch_Controller extends Base_Controller
             $message = Swift_Message::newInstance('一个定位于上海的免费婚恋交友平台：seekfortruelove.org')
                         ->setFrom(array(Config::get('application.mail_account') => 'SEEKFORTRUELOVE'))
                         ->setTo(array($email => $email))
-                        ->setBody($messageBody,'text/html');
-
+                        ->setBody($GLOBALS['messageBody'],'text/html');
+            $mailer = & $GLOBALS['mailer'];
             $status = $mailer->send($message);
             var_dump($status);// 成功就是1    
         }
