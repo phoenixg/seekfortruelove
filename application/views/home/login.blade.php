b8f709a9f1160cbdd95acd67d17bdf73d9ba4f14
phx-hp1000 <306761352@qq.com>
2013-06-18 21:47:37 +0800

    不允许未激活邮箱的用户登录

--- application/views/home/login.blade.php
+++ application/views/home/login.blade.php
@@ -2,40 +2,47 @@
 
 @section('content')
 <div class="container">
-	<h1>用户登录</h1>
+    <h1>用户登录</h1>
 
-	<div class="row">
-		<div class="span12">
-			{{ Form::open(URL::to_route('login'), 'POST', array('class' => 'form-horizontal')) }}
-				<fieldset>	
-					<legend></legend>				
-					<div class="control-group">
-						{{ Form::label('email', '电子邮件', array('class' => 'control-label')) }}
-						<div class="controls">
-							<input type="text" name="email" placeholder="eg. user@domain.com" class="span3">
-						</div>
-					</div>
+    <div class="row">
+        <div class="span12">
+            @if(Session::has('error_message'))
+            <div class="alert alert-warning">
+                <a data-dismiss="alert" class="close">×</a>
+                {{ Session::get('error_message') }}
+            </div>
+            @endif
+            
+            {{ Form::open(URL::to_route('login'), 'POST', array('class' => 'form-horizontal')) }}
+                <fieldset>  
+                    <legend></legend>               
+                    <div class="control-group">
+                        {{ Form::label('email', '电子邮件', array('class' => 'control-label')) }}
+                        <div class="controls">
+                            <input type="text" name="email" placeholder="eg. user@domain.com" class="span3">
+                        </div>
+                    </div>
 
-					<div class="control-group">
-						{{ Form::label('password', '登录密码', array('class' => 'control-label')) }}
-						<div class="controls">
-							<input type="password" name="password" placeholder="" class="span3">
-						</div>
-					</div>
+                    <div class="control-group">
+                        {{ Form::label('password', '登录密码', array('class' => 'control-label')) }}
+                        <div class="controls">
+                            <input type="password" name="password" placeholder="" class="span3">
+                        </div>
+                    </div>
 
-					<div class="control-group">
-						<div class="controls">
-							{{ Form::submit('登录', array('class' => 'btn btn-primary')) }}
-						</div>
-					</div>
-				
-				</fieldset>
+                    <div class="control-group">
+                        <div class="controls">
+                            {{ Form::submit('登录', array('class' => 'btn btn-primary')) }}
+                        </div>
+                    </div>
+                
+                </fieldset>
 
-				{{ Form::token() }}
+                {{ Form::token() }}
 
-			{{ Form::close() }}
+            {{ Form::close() }}
 
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 @endsection
